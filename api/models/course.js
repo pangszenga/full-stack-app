ee61b32bd92a71a93ef156a6874b80900cf55401
Priscilla Pang <41451092+ppdevelopmentinc@users.noreply.github.com>
2019-10-02 14:52:31 +0100

    Update Data type of Description

--- api/models/course.js
+++ api/models/course.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
         }
       },
       description: {
-        type: DataTypes.TEXT,
+        type: DataTypes.STRING,
         validate: {
           // allowNull: false,
           notEmpty: {
